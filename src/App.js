b8e2b4067fd1333cd00a3f25d3b4779cf311f5f4
Tom <84847318+tomal02@users.noreply.github.com>
2022-08-02 22:50:04 +0100

    initialised base react app with tailwind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Coming Soon</h2>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
