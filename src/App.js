13628fc178fdea0c87f713b6ac853dd4f3a530a2
Tom <84847318+tomal02@users.noreply.github.com>
2022-08-04 13:38:47 +0100

    started basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="bg-black h-screen w-full flex justify-center items-center flex-col">
+      <div className="h-32 bg-red-500">
+        <h1 className="text-white">Navbar</h1>
+      </div>
+      <div className=" h-full bg-slate-600">
+        <h1 className=" text-white">Hi, I'm Tom</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
