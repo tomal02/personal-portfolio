f878d87fcb135c1393fb6e21f12b5e043612c6cb
Tom <tomadamlee02@gmail.com>
2022-10-13 11:30:27 +0100

    adds template project data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: 'Kusari',
+    subtitle: 'React, Django, PostgreSQL',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-1.gif',
+    link: 'https://reactbootcamp.com',
+  },
+  {
+    title: 'Molesey Childminder',
+    subtitle: 'React',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-2.gif',
+    link: 'https://reedbarger.com',
+  },
+  {
+    title: 'IOBook',
+    subtitle: 'Rails',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-3.gif',
+    link: 'https://jsbootcamp.com',
+  },
+  {
+    title: 'Dream Log App (WIP)',
+    subtitle: 'React, React Native',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-4.gif',
+    link: 'https://pythonbootcamp.com',
+  },
+];
