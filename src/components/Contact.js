8ff60a400dff0c76801bb8b4a93c1fce9f9c46fa
Tom <tomadamlee02@gmail.com>
2022-10-13 13:37:17 +0100

    updates contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -70,11 +70,10 @@ const Contact = () => {
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Contact Me
           </h2>
           <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            Have a question or have some work for me? Feel free to reach out.
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
