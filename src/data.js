5b429bf2dfb1a6b9a2a2dd89b877d79b6339b11d
Tom <tomadamlee02@gmail.com>
2022-10-13 13:36:59 +0100

    updates skills and projects

--- src/data.js
+++ src/data.js
@@ -3,32 +3,42 @@ export const projects = [
     title: 'Kusari',
     subtitle: 'React, Django, PostgreSQL',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    image: './project-1.gif',
-    link: 'https://reactbootcamp.com',
+      "Named after the Japanese word for 'chain', this full-stack blockchain application sets out to consolidate investors cryptocurrency investments into one application. This project provided a perfect opportunity to try new technologies and step out of my comfort zone.",
+    image: './kusari_screenshot.jpg',
+    link: 'https://github.com/EMDevelop/Kusari',
   },
   {
     title: 'Molesey Childminder',
     subtitle: 'React',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    image: './project-2.gif',
-    link: 'https://reedbarger.com',
+      'A modern and professional front for Molesey Childminder, a childminding business. It also has a parent portal, for viewing sensitive information, which is protected using Userfront authorisation.',
+    image: './MC-scrollthrough.gif',
+    link: 'https://www.moleseychildminder.com/',
   },
   {
     title: 'IOBook',
     subtitle: 'Rails',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    image: './project-3.gif',
-    link: 'https://jsbootcamp.com',
+      'Named to convey the nature of this programmer-centric social network, this was my first experience with Rails. I implemented OmniAuth into this project to streamline the user sign in experience.',
+    image: './iobook.png',
+    link: 'https://github.com/tomal02/iobook',
   },
   {
     title: 'Dream Log App (WIP)',
     subtitle: 'React, React Native',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    image: './project-4.gif',
+      'A full-stack application for logging dreams, with a React Native front-end. This project is still in development, but I am using it to learn React Native and to experiment with different technologies.',
+    image: 'https://picsum.photos/seed/picsum/200/300?blur=2',
     link: 'https://pythonbootcamp.com',
   },
+];
+
+export const skills = [
+  'HTML & CSS',
+  'JavaScript & Frameworks (React, Express, Node)',
+  'Ruby & Rails',
+  'Python & Django',
+  'MongoDB',
+  'SQL',
+  'Git',
 ];
