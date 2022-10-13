4dce204469e8f4de278bfc357b645600a892e335
Tom <tomadamlee02@gmail.com>
2022-10-13 10:27:36 +0100

    basic portfolio structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
 function App() {
   return (
-    <div className="bg-black h-screen w-full flex justify-center items-center flex-col">
-      <div className="h-32 bg-red-500">
-        <h1 className="text-white">Navbar</h1>
-      </div>
-      <div className=" h-full bg-slate-600">
-        <h1 className=" text-white">Hi, I'm Tom</h1>
-      </div>
-    </div>
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
   );
 }
 
