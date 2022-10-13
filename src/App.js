ad2b3dc00b5d9f96fbb99c170b79751ed1ceded0
Tom <tomadamlee02@gmail.com>
2022-10-13 13:36:48 +0100

    adds skills section to app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
+      <Skills />
       <Projects />
       <Contact />
     </main>
