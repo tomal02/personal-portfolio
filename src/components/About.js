4fd4988dfb92a969cead97d08eb2a3bfd9e9ee01
Tom <tomadamlee02@gmail.com>
2023-02-03 23:34:40 +0000

    updates image of me

--- src/components/About.js
+++ src/components/About.js
@@ -33,9 +33,9 @@ const About = () => {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            className="object-cover object-center rounded"
+            className="object-cover object-center rounded-lg"
             alt="hero"
-            src="./me_boat_photo_cropped_blurred.JPG"
+            src="./portfolio_image.jpg"
           />
         </div>
       </div>
