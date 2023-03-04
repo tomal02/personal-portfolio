e043988d6777b4c6ae811b49464b77a3bb406540
Tom <tomadamlee02@gmail.com>
2023-03-04 21:23:39 +0000

    removes image of me and adjusts styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react';
 const About = () => {
   return (
     <section id="about" className='scroll-mt-20'>
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <div className="container mx-auto flex px-5 pt-20 py-10">
+        <div className="lg:flex-grow md:w-1/2  flex flex-col  mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Tom.
             <br className="hidden lg:inline-block" />
@@ -28,13 +28,13 @@ const About = () => {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded-lg"
             alt="hero"
             src="./portfolio_image.jpg"
           />
-        </div>
+        </div> */}
       </div>
     </section>
   );
